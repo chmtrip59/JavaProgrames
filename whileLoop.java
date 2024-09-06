bb9030c0261713e0d82ec245feca5a01900d00c4
chmtrip59 <chmtrip59@gmail.com>
2024-09-06 13:30:53 +0530

    While Loop

---
+++ whileLoop.java
@@ -0,0 +1,46 @@
+
+
+public class whileLoop {
+    public static void main(String[] args) {
+    
+    //print number from 0 to 49 ie 50
+    
+        //    int i=0;
+        // while(i<50){
+        //     System.out.println(i);
+        //     i = i+1;
+           
+        // }
+        //print number from 1 to 50
+        //    int i=1;
+        // while(i<=50){
+        //     System.out.println(i);
+        //     i = i+1;
+           
+        // }
+        
+        
+        
+        //Print number from 50 to 5 in descending order
+
+        // int count = 50;
+        // while(count >= 5){
+        //     System.out.println(count);
+        //     // count = count - 1;
+        //     count--;
+        // }
+        // Get input from user multiple times using While Loop
+        // Scanner input = new Scanner(System.in);
+
+        // int i = 0;
+
+        // while(i<3){
+        //     int getNumber = input.nextInt();
+        //     System.out.println("Number is : "+getNumber);
+        //     i++;
+        // }
+
+
+    }
+
+}
