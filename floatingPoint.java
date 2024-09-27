9db194dbe6c51ae480a3187f9eb060560348f8c9
chmtrip59 <chmtrip59@gmail.com>
2024-09-27 17:25:16 +0530

    updt

--- floatingPoint.java
+++ floatingPoint.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class floatingPoint{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter first floating Number: ");
+        System.out.print("Please enter first floating Number: ");
         double first = in.nextDouble();
-        System.out.println("Please enter Second floating Number: ");
+        System.out.print("Please enter Second floating Number: ");
         double second = in.nextDouble();
         
         double mul = first*second;
 
-        System.out.println("\n Result: "+ mul);
+        System.out.println("\nResult: "+ mul);
     }
 
 }
